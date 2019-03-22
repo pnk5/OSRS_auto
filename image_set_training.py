5afa378b0aad4302bc7379d29c548376f96fc9a2
Patrick Kidd <pnk5@rice.edu>
2019-03-22 13:01:48 -0500

    Add tensorflow models.

--- image_set_training.py
+++ image_set_training.py
@@ -1,11 +1,17 @@
-import tensorflow-gpu as tf
+import tensorflow as tf
 import os
 import sys
+import json
 from object_detection.utils import dataset_util
 
 flags = tf.app.flags
 flags.DEFINE_string('trained_data', '', '')
 FLAGS = flags.FLAGS
+
+json_file = open('json1')
+json_str = json_file.read()
+data = json.loads(json_str)[0]
+print(data)
 
 def create_dataset(image, payload):
     height = 1040
@@ -46,4 +52,20 @@ def create_dataset(image, payload):
     }))
 
     return tf_set
+
+
+# def main(_):
+#     writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
+#
+#     # TODO(user): Write code to read in your dataset to examples variable
+#
+#     for example in examples:
+#         tf_example = create_tf_example(example)
+#         writer.write(tf_example.SerializeToString())
+#
+#     writer.close()
+#
+#
+if __name__ == '__main__':
+    tf.app.run()
 
